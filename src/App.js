b3f9fdd0eda793376dc986a228119b2fa34d0be4
KRISHNA MURALI <krishna23994@gmail.com>
2025-07-20 19:52:25 -0400

    fix issues

--- src/App.js
+++ src/App.js
@@ -13,12 +13,33 @@ function App() {
       console.log('App component unmounted');
     };
   }, []);
+
+  const anotherTracedEvent = (data) => {
+    tracedEvent(data,async ({params,traceInfo}) => { 
+         log.info('Pay now button clicked with params in anotherTracedEvent', params);
+
+        throw new Error('Simulated error for testing'); // Simulating an error
+      },async ({error,traceInfo})=>{
+        console.log("Show modal for traceId in anotherTracedEvent:", traceInfo?.traceId);
+        log.error(`Error handler called: in anotherTracedEvent ${error?.message}`);
+      },false);
+  }
+
+  const handleClick = (e) => {
+    tracedEvent({test: "data"},async ({params,traceInfo}) => { 
+      log.info('Pay now button clicked with params in handleClick', params);
+
+      anotherTracedEvent(params);
+      
+    },async ({error,traceInfo})=>{
+      console.log("Show modal for traceId:", traceInfo?.traceId);
+       log.error(`Error handler called: in handleClick ${error?.message}`);
+    },false);
+  }
+
   return (
     <div className="App">
-      <button onClick={() => { tracedEvent("user-clicked-payment", () => { log.info('Payment button clicked'); 
-         log.info('Payment made');
-
-      }) }}>Pay now</button>
+        <button onClick={handleClick}>Pay now</button>
     </div>
   );
 }
